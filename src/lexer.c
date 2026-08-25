85bfaba88a4c03774d5ce75ed88a28e5f4bc6f49
sunain-s <sas@drsyed.org>
2026-08-25 15:43:13 +0100

    feat(lexer): static helpers current(), peek(), advance()

--- src/lexer.c
+++ src/lexer.c
@@ -7,3 +7,24 @@ struct Lexer {
     int line;
     int column;
 };
+
+static char lexer_current(Lexer *lexer) {
+    return lexer->source[lexer->position];
+}
+
+static char lexer_peek_next(Lexer *lexer) {
+    if (lexer->source[lexer->position + 1] == '\0') {
+        return '\0';
+    }
+    return lexer->source[lexer->position + 1];
+}
+
+static void lexer_advance(Lexer *lexer) {
+    if (lexer_current(lexer) == '\n') {
+        lexer->line++;
+        lexer->column = 1;
+    } else {
+        lexer->column++;
+    }
+    lexer->position++;
+}
