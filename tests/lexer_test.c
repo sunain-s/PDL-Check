ac4ba5f520c877b73108bd41907d1ec2e5853a9f
sunain-s <sas@drsyed.org>
2026-08-26 20:20:47 +0100

    test(lexer): test runner framework + testing identifiers

--- tests/lexer_test.c
+++ tests/lexer_test.c
@@ -39,3 +39,61 @@ static int tests_failed = 0;
             return; \
         } \
     } while (0)
+
+static void assert_token(Token token, TokenType expected_type, const char *expected_lexeme, int expected_line, int expected_column) {
+    ASSERT_INT(expected_type, token.type);
+    ASSERT_STRING(expected_lexeme, token.lexeme);
+    ASSERT_INT(expected_line, token.line);
+    ASSERT_INT(expected_column, token.column);
+
+    free(token.lexeme);
+}
+
+static Token next_token(Lexer *lexer) {
+    return lexer_next_token(lexer);
+}
+
+//-------------------------------------------------------------------------------------------------
+// Tests
+
+static void test_identifiers(void) {
+    Lexer *lexer = lexer_create("foo bar _temp hello_world x123");
+    ASSERT_TRUE(lexer != NULL);
+
+    assert_token(next_token(lexer), TOKEN_IDENTIFIER, "foo", 1, 1);
+    assert_token(next_token(lexer), TOKEN_IDENTIFIER, "bar", 1, 5);
+    assert_token(next_token(lexer), TOKEN_IDENTIFIER, "_temp", 1, 9);
+    assert_token(next_token(lexer), TOKEN_IDENTIFIER, "hello_world", 1, 15);
+    assert_token(next_token(lexer), TOKEN_IDENTIFIER, "x123", 1, 27);
+    assert_token(next_token(lexer), TOKEN_EOF, "", 1, 31);
+
+    lexer_destroy(lexer);
+}
+
+//-------------------------------------------------------------------------------------------------
+// Test runner
+
+typedef void (*TestFunction)(void);
+
+typedef struct {
+    const char *name;
+    TestFunction function;
+} Test;
+
+static Test tests[] = {
+    {"identifiers", test_identifiers}
+};
+
+int main(void) {
+    size_t test_count = sizeof(tests) / sizeof(tests[0]);
+    for (size_t i = 0; i < test_count; i++) {
+        int failures_before = tests_failed;
+        tests[i].function();
+
+        if (tests_failed == failures_before) {
+            printf("PASS: %s\n", tests[i].name);
+        }
+    }
+    printf("\n%d assertions, %d failed\n", tests_run, tests_failed);
+    return tests_failed == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
+}
